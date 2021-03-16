138d69c717900262f7394bf07a59294a4f711070
zlhunzzing <zlhunzzing@gmail.com>
2021-03-16 14:15:38 +0900

    사운드 url 수정
    사운드 폴더 날림으로 인한 url 재재수정

--- client/src/component/Sound.tsx
+++ client/src/component/Sound.tsx
@@ -4,13 +4,13 @@ export default function Sound() {
   return (
     <span>
       {/* <audio className='Sound' src=''></audio> */}
-      <audio className='moveSound' src='https://nymph-of-gladia-s3.s3.ap-northeast-2.amazonaws.com/sound_nog/406742__kretopi__steponwood-010.wav'></audio>
-      <audio className='manaUpSound' src='https://nymph-of-gladia-s3.s3.ap-northeast-2.amazonaws.com/sound_nog/346116__lulyc__retro-game-heal-sound.wav'></audio>
+      <audio className='moveSound' src='https://magic-colosseum-s3.s3.ap-northeast-2.amazonaws.com/bgm/406742__kretopi__steponwood-010.wav'></audio>
+      <audio className='manaUpSound' src='https://magic-colosseum-s3.s3.ap-northeast-2.amazonaws.com/bgm/346116__lulyc__retro-game-heal-sound.wav'></audio>
       <audio className='hitSound' src='https://zlhunzzing.net/wp-content/uploads/2019/12/118513__thefsoundman__punch-02.wav'></audio>
-      <audio className='guardSound' src='https://nymph-of-gladia-s3.s3.ap-northeast-2.amazonaws.com/sound_nog/370203__nekoninja__shield-guard.wav'></audio>
-      <audio className='winSound' src='https://nymph-of-gladia-s3.s3.ap-northeast-2.amazonaws.com/sound_nog/521949__kastenfrosch__success-jingle.ogg'></audio>
-      <audio className='loseSound' src='https://nymph-of-gladia-s3.s3.ap-northeast-2.amazonaws.com/sound_nog/370209__jugraf__fail-down.wav'></audio>
-      <audio className='drawSound' src='https://nymph-of-gladia-s3.s3.ap-northeast-2.amazonaws.com/sound_nog/504609__neospica__drawing-knife.wav'></audio>
+      <audio className='guardSound' src='https://magic-colosseum-s3.s3.ap-northeast-2.amazonaws.com/bgm/370203__nekoninja__shield-guard.wav'></audio>
+      <audio className='winSound' src='https://magic-colosseum-s3.s3.ap-northeast-2.amazonaws.com/bgm/521949__kastenfrosch__success-jingle.ogg'></audio>
+      <audio className='loseSound' src='https://magic-colosseum-s3.s3.ap-northeast-2.amazonaws.com/bgm/370209__jugraf__fail-down.wav'></audio>
+      <audio className='drawSound' src='https://magic-colosseum-s3.s3.ap-northeast-2.amazonaws.com/bgm/504609__neospica__drawing-knife.wav'></audio>
     </span>
   );
 }
